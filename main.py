3e2204dcc64d70110e1986a916e9c85e3d48a051
ShayaanUsmani <shayaanusmani@gmail.com>
2023-10-19 12:58:14 -0400

    Added viewing for saved workout plans and re-established database structure

--- main.py
+++ main.py
@@ -1,111 +1,177 @@
-from models import session, Workout, Exercise, Day
+from models import session, WorkoutPlan, Week, Day, Exercise
 
-# -------------------------data---------------------------------#
+# ------------------------- Data ------------------------- #
+
+# Constants for exercise types
 COMPOUND = 1
 ACCESSORY = 2
 MOBILITY = 3
 CARDIO = 4
 
-compound_exercises = ['Bench Press', 'Squat', 'Deadlift', 'Overhead Press', 'Lat Pull Down', 'Cable Row']
-accessory_exercises = ['Lateral Raise', 'Bicep Curl', 'Tricep Pushdown', 'Overhead Tricep Extension', 'Rear Delt Fly',
+# Lists of example exercises for each type
+COMPOUND_EXERCISES = ['Bench Press', 'Squat', 'Deadlift', 'Overhead Press', 'Lat Pull Down', 'Cable Row']
+ACCESSORY_EXERCISES = ['Lateral Raise', 'Bicep Curl', 'Tricep Pushdown', 'Overhead Tricep Extension', 'Rear Delt Fly',
                        'Leg Curl', 'Leg Extension', 'Calf Raises']
-mobility_exercises = ['Something']
-cardio_exercises = ['Treadmill', 'Bike']
+MOBILITY_EXERCISES = ['Something']
+CARDIO_EXERCISES = ['Treadmill', 'Bike']
 
 
-# ----------------------functions-------------------------------#
+# ------------------------- Functions ------------------------- #
+
+def get_user_input_for_exercise(exercise_name, is_cardio):
+    """Get user input for sets and reps (or duration for cardio)."""
+    print()
+    if is_cardio:
+        sets = 1
+        reps = int(input(f"How long (in minutes) for {exercise_name}? "))
+    else:
+        sets = int(input(f"How many sets of {exercise_name}? "))
+        reps = int(input("How many reps per set? "))
+    return [sets, reps]
+
 
 def welcome_menu():
+    """Display the main welcome menu and handle user choices."""
     print("Welcome to the Workout Program App")
     print("1. Create a new workout program")
     print("2. View saved workout programs")
     print("3. Exit")
-    choice = input("\nChoose from 1-3: ")
-    if choice == '1':
-        add_workout_program()
-    elif choice == '2':
-        view_workouts()
-    elif choice == '3':
-        exit()
-    else:
-        print("Invalid choice. Please try again.")
+
+    actions = {
+        '1': add_workout_program,
+        '2': view_workouts,
+        '3': exit
+    }
+
+    while True:
+        choice = input("Choose from 1-3: ")
+
+        action = actions.get(choice)
+        if action:
+            action()
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 
-def add_exercise(exercise_list):
+def add_exercise(exercise_list, new_day_id):
     print()
     for i in range(len(exercise_list)):
-        print(str(i + 1) + ". " + exercise_list[i])
+        print(f"{i + 1}. {exercise_list[i]}")
     print()
+
     while True:
         try:
-            choice = int(input("Choose from 1-" + str(len(exercise_list)) + ": "))
+            choice = int(input(f"Choose from 1-{str(len(exercise_list))}: "))
             if not (1 <= choice <= len(exercise_list)):
                 raise ValueError
 
             exercise_name = exercise_list[choice - 1]
+            sets_x_reps = get_user_input_for_exercise(exercise_name, exercise_list == CARDIO_EXERCISES)
+            new_exercise = Exercise(name=exercise_name, sets=sets_x_reps[0], reps=sets_x_reps[1], day_id=new_day_id)
+            session.add(new_exercise)
 
-            if exercise_list is cardio_exercises:
-                sets = 1
-                reps = int(input("How long (in minutes) for " + exercise_name + "? "))
-            else:
-                sets = int(input("How many sets of " + exercise_name + "? "))
-                reps = int(input("How many reps per set? "))
             break
 
         except ValueError:
             print("Invalid entry, please try again")
-
-    return Exercise(name=exercise_list[choice - 1], sets=sets, reps=reps)
-
+        session.commit()
 
 def add_workout_program():
-    workout_name = input("\nEnter the name of the workout: ")
-    duration = int(input("Enter Cycle Duration in weeks (number of weeks in program): "))
-    frequency = int(input("Enter Frequency per week (number of workout days per week): "))
+    """Allow the user to create a new workout program."""
+    name = input("Enter the name of the new workout program: ")
+    duration_weeks = int(input("How many weeks will this program last? "))
+    frequency_days = int(input("How many days per week? "))
 
-    # Create a new Workout instance
-    new_workout = Workout(name=workout_name, duration_weeks=duration, frequency_days=frequency)
-    session.add(new_workout)
+    new_workout_plan = WorkoutPlan(name=name, duration_weeks=duration_weeks, frequency_days=frequency_days)
+    session.add(new_workout_plan)
     session.commit()
 
-    # Loop through the number of days and allow user to add exercises for each day
-    for day_num in range(1, frequency + 1):
-        print("\nDay " + str(day_num))
-        new_day = Day(day_number=day_num, workout=new_workout)
-        session.add(new_day)
-        while True:
-            print("1. Add compound/main movement")
-            print("2. Add accessory/isolation movement")
-            print("3. Add mobility movement")
-            print("4. Add cardio")
-            print("5. Next day")
-            print("6. Exit")
-
-            choice = input("\nChoose from 1-6: ")
-
-            if choice == "1":
-                new_exercise = add_exercise(compound_exercises)
-            elif choice == "2":
-                new_exercise = add_exercise(accessory_exercises)
-            elif choice == "3":
-                new_exercise = add_exercise(mobility_exercises)
-            elif choice == "4":
-                new_exercise = add_exercise(cardio_exercises)
-            elif choice == "5":
-                break
-            elif choice == "6":
-                return
-
-            new_exercise.day_id = new_day.id
-            session.add(new_exercise)
+    for week_number in range(1, duration_weeks):
+        # Adding the initial week separately so the user only has to input their exercises for one week
+        # Data will be copied to other weeks later
+        initial_week = Week(week_number=1, workout_plan_id=new_workout_plan.id)
+        session.add(initial_week)
         session.commit()
+
+        if week_number == 1:
+            for day_number in range(1, frequency_days + 1):
+                new_day = Day(day_number=day_number, week_id=initial_week.id)
+                session.add(new_day)
+                session.commit()
+                new_day_id = new_day.id
+                while True:
+                    print("What type of exercise would you like to add?")
+                    print("1. Compound")
+                    print("2. Accessory")
+                    print("3. Mobility")
+                    print("4. Cardio")
+                    print("5. Finish Day")
+                    print("6. Finish Program")
+                    choice = input(f"Choose from 1-6: ")
+
+                    if choice == '1':
+                        add_exercise(COMPOUND_EXERCISES, new_day_id)
+                    elif choice == '2':
+                        add_exercise(ACCESSORY_EXERCISES, new_day_id)
+                    elif choice == '3':
+                        add_exercise(MOBILITY_EXERCISES, new_day_id)
+                    elif choice == '4':
+                        add_exercise(CARDIO_EXERCISES, new_day_id)
+                    elif choice == '5':
+                        break
+                    elif choice == '6':
+                        return
+                    else:
+                        print("Invalid choice. Please try again.")
+        else:
+            new_week = Week(week_number=week_number, workout_plan_id=new_workout_plan.id)
+            session.add(new_week)
+            session.commit()
+            for day_number in range(1, frequency_days + 1):
+                new_day = Day(day_number=day_number, week_id=new_week.id)
+                session.add(new_day)
+                session.commit()
+                for exercise in range(initial_week.days.exercises):
+                    new_exercise = Exercise(name=exercise.name, sets=exercise.sets, reps=exercise.reps,
+                                            day_id=new_day.id)
+                    session.add(new_exercise)
+                    session.commit()
 
 
 def view_workouts():
-    workouts = session.query(Workout).all()
-    for workout in workouts:
-        print(str(workout.id) + ". " + workout.name)
+    """View all saved workout programs."""
+    workout_plans = session.query(WorkoutPlan).all()
+    weeks = session.query(Week).all()
+    days = session.query(Day).all()
+    exercises = session.query(Exercise).all()
+    if not workout_plans:
+        print("No workout programs found.")
+        return
+
+    print("Saved Workout Programs:")
+    for i, workout_plan in enumerate(workout_plans, start=1):
+        print(f"{i}. {workout_plan.name}")
+
+    choice = int(input(f"Choose from 1-{len(workout_plans)}: "))
+    if 1 <= choice <= len(workout_plans):
+        for week in weeks:
+            print(f"Week {week.week_number}")
+            for day in days:
+                print(f"Day {day.day_number}")
+                for exercise in exercises:
+                    print(f"{exercise.name}: {exercise.sets} x {exercise.reps}")
 
 
-# -------------------------MAIN--------------------------------------------#
-welcome_menu()
+
+def exit():
+    """Exit the program."""
+    print("Goodbye!")
+    session.close()
+    quit()
+
+
+# ------------------------- Main ------------------------- #
+
+if __name__ == "__main__":
+    welcome_menu()
--- models.py
+++ models.py
@@ -1,34 +1,61 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+# Database setup
+engine = create_engine("sqlite:///workout_programs.db")
 Base = declarative_base()
-
-class Workout(Base):
-    __tablename__ = 'workouts'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    duration_weeks = Column(Integer)
-    frequency_days = Column(Integer)
-    days = relationship('Day', backref='workout')
-
-class Day(Base):
-    __tablename__ = 'days'
-    id = Column(Integer, primary_key=True)
-    day_number = Column(Integer)
-    workout_id = Column(Integer, ForeignKey('workouts.id'))
-    exercises = relationship('Exercise', backref='day')
-
-class Exercise(Base):
-    __tablename__ = 'exercises'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    sets = Column(Integer)
-    reps = Column(Integer)
-    day_id = Column(Integer, ForeignKey('days.id'))
-
-DATABASE_URL = "sqlite:///workout.db"
-engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+# Define the WorkoutPlan class
+class WorkoutPlan(Base):
+    """Represents a workout program."""
+    __tablename__ = 'workout_plans'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    duration_weeks = Column(Integer, nullable=False)
+    frequency_days = Column(Integer, nullable=False)
+
+    weeks = relationship('Week', back_populates='workout_plan')
+
+class Week(Base):
+    """Represents a week within a workout program."""
+    __tablename__ = 'weeks'
+
+    id = Column(Integer, primary_key=True)
+    week_number = Column(Integer, nullable=False)
+    workout_plan_id = Column(Integer, ForeignKey('workout_plans.id'), nullable=False)
+
+    workout_plan = relationship('WorkoutPlan', back_populates='weeks')
+    days = relationship('Day', back_populates='week')
+
+class Day(Base):
+    """Represents a day within a week of a workout program."""
+    __tablename__ = 'days'
+
+    id = Column(Integer, primary_key=True)
+    day_number = Column(Integer, nullable=False)
+    week_id = Column(Integer, ForeignKey('weeks.id'), nullable=False)
+
+    week = relationship('Week', back_populates='days')
+    exercises = relationship('Exercise', back_populates='day')
+
+class Exercise(Base):
+    """Represents an exercise for a specific day in a workout program."""
+    __tablename__ = 'exercises'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    sets = Column(Integer, nullable=False)
+    reps = Column(Integer, nullable=False)
+    exercise_type = Column(Integer, nullable=True)
+    day_id = Column(Integer, ForeignKey('days.id'), nullable=False)
+
+    day = relationship('Day', back_populates='exercises')
+
+    weight = Column(Integer)
+    notes = Column(String)
+
+Base.metadata.create_all(engine)
